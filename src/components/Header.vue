1bb75098077a77a6024ffcea52181eff723cca71
Maccio92 <valerio362@gmail.com>
2022-01-24 15:01:40 +0100

    add slider

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,15 +3,15 @@
     <div class="container-fluid h-100">
         <div class="row row-cols-12 h-100 justify-content-between align-items-center">
         <img class="col-1" src="../assets/img/logo-img-01.png" alt="logo everlead">
-        <ul class="list-unstyled list-inline col-5 d-flex align-items-center m-0">
-                <li class="list-inline-item">homes</li>
-                <li class="list-inline-item">pages</li>
-                <li class="list-inline-item">blog</li>
-                <li class="list-inline-item">shop</li>
-                <li class="list-inline-item">events</li>
-                <li class="list-inline-item">elements</li>
-                <i class="fas fa-search "></i>
-                <i class="fas fa-list l"></i>
+        <ul class="list-unstyled list-inline col-5 d-flex align-items-center justify-content-end m-0">
+                <li class="list-inline-item mx-4">homes</li>
+                <li class="list-inline-item mx-4">pages</li>
+                <li class="list-inline-item mx-4">blog</li>
+                <li class="list-inline-item mx-4">shop</li>
+                <li class="list-inline-item mx-4">events</li>
+                <li class="list-inline-item mx-4">elements</li>
+                <i class="fas fa-search list-inline-item mx-4"></i>
+                <i class="fas fa-list list-inline-item mx-4"></i>
         </ul>
         </div>
     </div>
@@ -25,10 +25,9 @@ name: 'Header',
 </script>
 
 <style lang="scss">
-img{
-    width: 20%;
-}
 li{
     text-transform: uppercase;
+    font-size: 0.7em;
+    font-weight: 700;
 }
 </style>
