83133aaa4965b2f2b0c3b5ca28bf2f86498c34b0
Maccio92 <valerio362@gmail.com>
2022-01-26 12:36:23 +0100

    add feature component

---
+++ src/components/Features.vue
@@ -0,0 +1,18 @@
+<template>
+<li class="d-flex flex-column list-inline-item align-items-center col-3 p-3 m-0">
+    <img class="p-5" :src="img" alt="">
+    <h3 class="features-title">{{title}}</h3>
+    <p class="features-caption text-center">{{caption}}</p>
+</li>
+</template>
+
+<script>
+export default {
+name: 'Features',
+props: ['img', 'title', 'caption']
+}
+</script>
+
+<style lang="scss">
+
+</style>
